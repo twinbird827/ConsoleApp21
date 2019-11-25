195db8b07f9d77d227956d7987aaf7b8c056f04d
twinbird827 <twinbird827@gmail.com>
2019-11-25 14:11:12 +0900

    [add] exception 確認用

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                C();
+            }
+            catch (Exception e)
+            {
+                Output(e, "Main message");
+            }
+            Console.Read();
+        }
+
+        static void Output(Exception e, string message)
+        {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(message);
+            Console.WriteLine("*****");
+            Console.WriteLine(e.Message);
+            Console.WriteLine("*****");
+            Console.WriteLine(e.ToString());
+        }
+
+        static void A()
+        {
+            throw new Exception("A exception");
+        }
+
+        static void B()
+        {
+            try
+            {
+                A();
+            }
+            catch (Exception e)
+            {
+                Output(e, "B message");
+                throw new Exception("B exception", e);
+            }
+        }
+
+        static void C()
+        {
+            try
+            {
+                B();
+            }
+            catch (Exception e)
+            {
+                Output(e, "C message");
+                throw new Exception("C exception", e);
+            }
+        }
+    }
+}
